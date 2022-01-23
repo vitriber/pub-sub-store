33fd27491c2ca4904dc63502fd1f64badef5d0b7
Vitor <vit.riber1@hotmail.com>
2022-01-23 11:45:23 -0300

    implement report

--- services/report/app.js
+++ services/report/app.js
@@ -1,30 +1,46 @@
-const RabbitMQService = require('./rabbitmq-service')
-const path = require('path')
+const RabbitMQService = require("./rabbitmq-service");
+const path = require("path");
 
-require('dotenv').config({ path: path.resolve(__dirname, '.env') })
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
-var report = {}
-async function updateReport(products) {
-    for(let product of products) {
-        if(!product.name) {
-            continue
-        } else if(!report[product.name]) {
-            report[product.name] = 1;
-        } else {
-            report[product.name]++;
-        }
+var report = {};
+function updateReport(products) {
+  for (let product of products) {
+    if (!product.name) {
+      continue;
+    } else if (!report[product.name]) {
+      report[product.name] = 1;
+    } else {
+      report[product.name]++;
     }
-
+  }
 }
 
-async function printReport() {
-    for (const [key, value] of Object.entries(report)) {
-        console.log(`${key} = ${value} vendas`);
-      }
+function printReport() {
+  for (const [key, value] of Object.entries(report)) {
+    console.log(`${key} = ${value} vendas`);
+  }
+}
+
+async function processMessage(msg) {
+  const orderData = JSON.parse(msg.content);
+  try {
+    updateReport(orderData.products);
+    printReport();
+  } catch (error) {
+    console.log(`X ERROR TO PROCESS: ${error.response}`);
+  }
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
-} 
+  console.log(
+    `INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`
+  );
+  await (
+    await RabbitMQService.getInstance()
+  ).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {
+    processMessage(msg);
+  });
+}
 
 consume()
